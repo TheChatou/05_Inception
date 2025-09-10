6a20964fedc4cccca7cf870f97b216ab55795b59
TheChatou <chatougna@gmail.com>
2025-09-10 15:30:21 +0200

    Update

--- srcs/builds/mariadb/dbStart.sh
+++ srcs/builds/mariadb/dbStart.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-echo "Starting MariaDB..."
+# echo "Starting MariaDB..."
 
-# DB_PWD=$(cat ${DB_PWD_FILE})
+DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+DB_PASSWORD=$(cat /run/secrets/db_password)
 
-# Start MariaDB
-# service mariadb start
+Start MariaDB
+service mariadb start
 
 
-# sleep 5
+sleep 5
 
-# mariadb -e "CREATE DATABASE IF NOT EXISTS \'${DB_NAME}\';"
-# mariadb -e "CREATE USER IF NOT EXISTS \'${DB_USER}\'@\'%\' IDENTIFIED BY \'${DB_PWD}\';"
-# mariadb -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO \'${DB_USER}\'@\'%\';"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \'${DB_NAME}\';"
+mariadb -e "CREATE USER IF NOT EXISTS \'${DB_USER}\'@\'%\' IDENTIFIED BY \'${DB_PASSWORD}\';"
+mariadb -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO \'${DB_USER}\'@\'%\';"
 
-# service mariadb stop
+service mariadb stop
 
-# exec mysqld --bind-address=0.0.0.0
-# Keep the container running
-# tail -f /dev/null
+exec mysqld --bind-address=0.0.0.0
+Keep the container running
+tail -f /dev/null
